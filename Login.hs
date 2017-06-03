df70e922405617a3a1970c73516299192bac9142
Matheus Fernandes <mcfernandes1110@gmail.com>
2017-06-03 19:20:43 +0000

    Adicionando sha256 como hash das senhas, e adicionando salt

--- Login.hs
+++ Login.hs
@@ -16,16 +16,21 @@ import Data.Text
 import Database.Persist.Postgresql
 import Foundation
 import Network.HTTP.Types.Status
+import Codec.Digest.SHA
+import Data.ByteString.Char8
+
+hashPassword :: Text -> Text
+hashPassword original = Data.Text.pack . showBSasHex . (hash SHA256) . Data.ByteString.Char8.pack $ "essa-fera-ai-meu" ++ (Data.Text.unpack original)
 
 postCadastroR :: Handler TypedContent
 postCadastroR = do
-    usuario <- requireJsonBody :: Handler Usuario -- nome, email e senha
-    uid <- runDB $ insert usuario
+    u <- requireJsonBody :: Handler Usuario -- nome, email e senha
+    uid <- runDB $ insert (Usuario (usuarioNome u) (usuarioEmail u) (hashPassword $ usuarioSenha u))
     sendStatusJSON created201 (object ["id" .= (fromSqlKey uid)])
 
 getLoginR :: Text -> Text -> Handler TypedContent
 getLoginR email senha = do
-    usuario <- runDB $ selectFirst [UsuarioEmail ==. email,UsuarioSenha ==. senha] []
+    usuario <- runDB $ selectFirst [UsuarioEmail ==. email,UsuarioSenha ==. (hashPassword senha)] []
     case usuario of
         Nothing -> do
             notFound
