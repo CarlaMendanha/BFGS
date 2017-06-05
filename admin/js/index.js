699e17137bd680ba3fa90c44ac8a43b5519d7705
Matheus Fernandes <mcfernandes1110@gmail.com>
2017-06-05 19:20:13 +0000

    Atualizando requisicoes do admin

--- admin/js/index.js
+++ admin/js/index.js
@@ -4,7 +4,12 @@ app.config(function($routeProvider) {
     $routeProvider
     .when("/", {
         templateUrl : "templates/main.html",
-        controller: "mainCtrl"
+        controller: "mainCtrl",
+        resolve: {
+            perguntas: function(Pergunta){
+                return Pergunta.all()
+            }
+        }
     })
     .when("/pergunta/nova", {
         templateUrl : "templates/pergunta/form.html",
@@ -22,8 +27,8 @@ app.config(function($routeProvider) {
             categorias: function(Categoria){
                 return Categoria.all();
             },
-            pergunta: function(Pergunta, $routeParams){
-                return Pergunta.get($routeParams.id)
+            pergunta: function(Pergunta, $route){
+                return Pergunta.get($route.current.params.id)
             },
         }
     })
@@ -31,8 +36,8 @@ app.config(function($routeProvider) {
         templateUrl : "templates/pergunta/show.html",
         controller: "showPergunta",
         resolve: {
-            pergunta: function(Pergunta, $routeParams){
-                return Pergunta.get($routeParams.id)
+            pergunta: function(Pergunta, $route){
+                return Pergunta.get($route.current.params.id)
             }
         }
     })
@@ -40,11 +45,35 @@ app.config(function($routeProvider) {
     .otherwise("/");
 });
 
-app.controller('mainCtrl', function($rootScope){
+app.run(function($rootScope){
+    $rootScope.dismiss = function(){
+        $rootScope.flashMsg = null;
+    }
+    $rootScope.showError = function(e){
+        console.log(e);
+        $rootScope.flashMsg = {
+            tipo: 'danger',
+            texto: 'Erro na requisição: '+e.statusText
+        }
+    }
+})
+
+app.controller('mainCtrl', function($rootScope, $scope, perguntas, Pergunta){
     $rootScope.titulo = 'Perguntas'
+    $scope.perguntas = perguntas;
+    $scope.remover = function(index){
+        if(confirm('Tem certeza que deseja remover esta pergunta?')){
+            var p = $scope.perguntas[index];
+            Pergunta.remove(p.id).then(function(r){
+                if(r){
+                    $scope.perguntas.splice(index, 1);
+                }
+            });
+        }
+    }
 });
 
-app.controller('novaPergunta', function($rootScope, $scope, $location, categorias){
+app.controller('novaPergunta', function($rootScope, $scope, $location, categorias, Pergunta){
     $rootScope.titulo = 'Nova pergunta';
     $scope.categorias = categorias;
     $scope.botaoSalvar = "Adicionar";
@@ -58,10 +87,26 @@ app.controller('novaPergunta', function($rootScope, $scope, $location, categoria
     };
     $scope.salvar = function(){
         var data = Object.assign({}, $scope.model);
+        if(!data.enunciado || !data.pontos || !data.categoria){
+            $rootScope.flashMsg = {
+                tipo: 'warning',
+                texto: 'Preencha todos os campos!'
+            }
+            return;
+        }
         data.categoriaId = data.categoria.id;
         delete data.categoria;
         delete data.id;
         console.log(data);
+        Pergunta.add(data).then(function(e){
+            if(e){
+                $rootScope.flashMsg = {
+                    tipo: 'success',
+                    texto: 'Pergunta cadastrada com sucesso!'
+                }
+                $location.path( "/" );
+            }
+        });
     }
 });
 
@@ -82,9 +127,9 @@ app.controller('editarPergunta', function($rootScope, $scope, $location, categor
     }
 });
 
-app.controller('showPergunta', function($rootScope, $scope, $routeParams, pergunta){
-    $rootScope.titulo = 'Pergunta #'+$routeParams.id;
-
+app.controller('showPergunta', function($rootScope, $scope, $routeParams, pergunta, Alternativa){
+    var perguntaId = parseInt($routeParams.id, 10);
+    $rootScope.titulo = 'Pergunta #'+perguntaId;
     $scope.pergunta = pergunta;
 
     $scope.alternativas = [
@@ -95,33 +140,78 @@ app.controller('showPergunta', function($rootScope, $scope, $routeParams, pergun
         var a = $scope.alternativas[index];
         var novoTexto = prompt("Digite o novo texto para a pergunta:");
         if(novoTexto.length > 0){
-            a.texto = novoTexto;
-            //TODO REQUISICAO
+            var data ={
+                certa: a.certa,
+                texto: novoTexto,
+                perguntaId: perguntaId
+            };
+            Alternativa.update(a.id, data).then(function(r){
+                if(r){
+                    a.texto = novoTexto;
+                }
+            })
         }
     }
     $scope.remover = function(index){
-        $scope.alternativas.splice(index, 1);
-        //TODO REQUISICAO
+        var a = $scope.alternativas[index];
+        if(confirm("Tem certeza que deseja remover esta alternativa?")){
+            Alternativa.remove(a.id).then(function(r){
+                if(r){
+                    $scope.alternativas.splice(index, 1);
+                }
+            })
+        }
     }
     $scope.marcar = function(index){
         var a = $scope.alternativas[index];
-        a.certa = !a.certa;
-        //TODO REQUISICAO
+        var atual = $scope.alternativas.find((x) => x.certa);
+        if(atual){
+            var data ={
+                certa: false,
+                texto: atual.texto,
+                perguntaId: perguntaId
+            };
+            Alternativa.update(a.id, data).then(function(r){
+                if(r){
+                    a.texto = novoTexto;
+                    atual.certa = false;
+                }
+            });
+        }
+        var data ={
+            certa: !a.certa,
+            texto: a.texto,
+            perguntaId: perguntaId
+        };
+        Alternativa.update(a.id, data).then(function(r){
+            if(r){
+                a.certa = !a.certa;
+            }
+        });
     }
-
 
     $scope.add = function(){
         var novoTexto = prompt("Digite o novo texto para a pergunta:");
         if(novoTexto.length > 0){
-            var a = {texto: novoTexto, certa: false};
-            //TODO REQUISICAO E SALVAR ID NA ALTERNATIVA
-            $scope.alternativas.push(a);
+            var data ={
+                certa: false,
+                texto: novoTexto,
+                perguntaId: perguntaId
+            };
+            Alternativa.add(data).then(function(r){
+                if(r){
+                    data.id = r.id;
+                    delete data.perguntaId;
+                    $scope.alternativas.push(data);
+                }
+            });
         }
     }
 });
 
-app.service('Categoria', function(){
+app.service('Categoria', function($http, $rootScope){
     this.all = function(){
+        //return $http.get('/categoria/').catch(console.error);
         return [
             {id : 1, nome : "Variedades"},
             {id : 2, nome : "Esportes"},
@@ -129,17 +219,34 @@ app.service('Categoria', function(){
     }
 });
 
-app.service('Pergunta', function(){
+app.service('Pergunta', function($http, $rootScope){
     this.get = function(id){
-        /*{
-          "pontos": 800,
-          "categoria": {
-            "nome": "Esportes",
-            "id": 1
-          },
-          "id": 2,
-          "enunciado": "tal tal"
-        }*/
-        return {id : 1, enunciado : "Qual a diferença entre o charme e o funk?", pontos: 3000, categoria: {id : 2, nome : "Esportes"}}
+        return $http.get('/pergunta/'+id).catch($rootScope.showError);
+        //return {id : 1, enunciado : "Qual a diferença entre o charme e o funk?", pontos: 3000, categoria: {id : 2, nome : "Esportes"}}
+    }
+    this.all = function(){
+        return [{id : 1, enunciado : "Qual a diferença entre o charme e o funk?", pontos: 3000, categoria: {id : 2, nome : "Esportes"}}]
+        return $http.get('/pergunta').catch($rootScope.showError);
+    }
+    this.update = function(id, data){
+        return $http.put('/pergunta/'+id, data).catch($rootScope.showError);
+    }
+    this.add = function(data){
+        return $http.post('/pergunta/', data).catch($rootScope.showError);
+    }
+    this.remove = function(id){
+        return $http.delete('/pergunta/'+id).catch($rootScope.showError);
+    }
+});
+
+app.service('Alternativa', function($http, $rootScope){
+    this.update = function(id, data){
+        return $http.put('/alternativa/'+id, data).catch($rootScope.showError);
+    }
+    this.add = function(data){
+        return $http.post('/alternativa/', data).catch($rootScope.showError);
+    }
+    this.remove = function(id){
+        return $http.delete('/alternativa/'+id).catch($rootScope.showError);
     }
 })
